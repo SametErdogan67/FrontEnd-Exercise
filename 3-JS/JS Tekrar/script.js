b56f29df61b432e48cc68ec3de6ac5b93fbd967a
SametErdogan <samedi6774@gmail.com>
2022-11-15 17:37:37 +0300

    clarusway Projeler

---
+++ 3-JS/JS Tekrar/script.js
@@ -0,0 +1,65 @@
+// // alert("Merhaba Dünya");
+
+// console.log("Samet Erdoğan");
+
+// console.log(15+"15");
+
+// let gun=4;
+// let ay=11
+// let yil = 1988;
+
+// let tarih = gun+"."+ay+"."+yil
+
+
+// let dogumTarihi = "Dogum tarihim "+tarih;
+
+// let date = dogumTarihi.toLocaleUpperCase();
+// let date2= date.toLocaleLowerCase();
+// let uzunluk2=date2.length;
+
+// console.log(date);
+// console.log(date2);
+// console.log(uzunluk2);
+// console.log(date[4]);
+
+
+// let adSoyad="Samet Erdoğan Zonguldak";
+
+// let bastanSona = adSoyad.slice(2,4);
+// console.log(bastanSona);
+
+// let bastanSona2 = adSoyad.substring(6,2);
+// console.log(bastanSona2);
+
+// let yerDegistirme = adSoyad.replace(" ", "-");
+// console.log(yerDegistirme);
+
+
+// let names= ["can", "rıdvan", "elif"];
+
+// console.log(names[0]);
+
+// names[0]="sam";
+
+// console.log(names);
+
+// console.log(names.length);
+
+// let tireli= names.join("-");
+// console.log(tireli);
+
+// let rows=names.indexOf("sa");
+// console.log(rows);
+
+// let added = names.concat("mike","jane");
+// console.log(added);
+
+// let elemanekle = added.push("like");
+// console.log(elemanekle);
+
+// let elemancikar= elemanekle.pop("");
+// console.log(elemancikar);
+
+let yas;
+
+console.log(yas,yas+5,`${yas}`);
